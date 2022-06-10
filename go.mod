61524063626a5830c00ecf25a9c9b6abf6c1a8c0
BrookeBryski <brookeabryski@gmail.com>
2022-06-10 14:18:48 -0700

    query for a single book

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Golang_Books_REST_Api
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Book struct
 type Book struct {
+	ID     string `json:"Id"`
 	Title  string `json:"Title"`
 	Author string `json:"Author"`
 	Pages  int    `json:"Pages"`
@@ -23,9 +26,11 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
 	// add our books route and map it to our returnAllBooks function
-	http.HandleFunc("/books", returnAllBooks)
+	myRouter.HandleFunc("/books", returnAllBooks)
+	myRouter.HandleFunc("/book/{id}", returnSingleBook)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
@@ -33,6 +38,18 @@ func returnAllBooks(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllBooks")
 	json.NewEncoder(w).Encode(Books)
 }
+
+func returnSingleBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, book := range Books {
+		if book.ID == key {
+			json.NewEncoder(w).Encode(book)
+		}
+	}
+}
+
 func main() {
 	Books = []Book{
 		Book{Title: "The Bell Jar",
